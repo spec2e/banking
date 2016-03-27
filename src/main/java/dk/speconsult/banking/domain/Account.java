c728b614998439eeedef4176c73a4361346f8018
Soren Pedersen <speconsult@gmail.com>
2016-03-27 18:22:06 +0200

    a little test setup

--- src/main/java/dk/speconsult/banking/domain/Account.java
+++ src/main/java/dk/speconsult/banking/domain/Account.java
@@ -1,5 +1,7 @@
 package dk.speconsult.banking.domain;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -13,13 +15,18 @@ public class Account {
 
     AccountTerms accountTerms;
 
-    List<Posting> postings;
+    List<Posting> postings = new ArrayList<Posting>();
 
+    public Account(AccountNumber accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 
     public void depositAmount(Amount amount, String description) {
         // build a Deposit object...
         // add it to the deposits
         // increments then interests
+        Posting posting = new Posting(amount, description, new Date());
+        postings.add(posting);
     }
 
     public long calculateInterest() {
@@ -27,4 +34,19 @@ public class Account {
     }
 
 
+    public List<Posting> getPostings() {
+        return postings;
+    }
+
+    public AccountNumber getAccountNumber() {
+        return accountNumber;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public AccountTerms getAccountTerms() {
+        return accountTerms;
+    }
 }
---
+++ src/test/java/dk/speconsult/banking/control/MakeDepositTest.java
@@ -0,0 +1,53 @@
+package dk.speconsult.banking.control;
+
+import dk.speconsult.banking.domain.Account;
+import dk.speconsult.banking.domain.AccountNumber;
+import dk.speconsult.banking.domain.Posting;
+import dk.speconsult.banking.domain.SSN;
+import dk.speconsult.banking.interfaces.AccountDatabase;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by zapp on 27/03/16.
+ */
+public class MakeDepositTest {
+
+    private MakeDeposit makeDeposit;
+    private String depositDescription;
+
+    @Before
+    public void setUp() throws Exception {
+        makeDeposit = new MakeDeposit();
+        makeDeposit.accountDatabase = new AccountDatabase() {
+            public void saveAccount(Account account) {
+                Posting posting = account.getPostings().get(0);
+                depositDescription = posting.getDescription();
+            }
+
+            public Account fetchAccount(AccountNumber acountNumber) {
+                return new Account(acountNumber);
+            }
+
+            public List<Account> fetchAccountsForUser(SSN ssn) {
+                return null;
+            }
+        };
+    }
+
+    @Test
+    public void whenNull_thenException() {
+        // some test....
+    }
+
+    @Test
+    public void whenAnotherTest_thenSomeResult() {
+        MakeDepositRequest request = new MakeDepositRequest("1234", 200l, "desc");
+        makeDeposit.execute(request);
+        assertEquals("desc", depositDescription);
+    }
+}
